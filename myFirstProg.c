1bebe449b3da5bfaedf743a6af80e3f8949819eb
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-06-26 21:50:40 +0700

    star

--- myFirstProg.c
+++ myFirstProg.c
@@ -2,19 +2,14 @@
 
 int main()
 {
-    // 123 / 10 = 12
-    int x;
-    int sum = 0;
-    scanf("%d", &x);
-    int temp = x;
-
-    while (temp > 0)
+    for (int i = 0; i < 3; i++)
     {
-        sum = sum + (temp % 10);
-        temp /= 10;
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
     }
-
-    printf("%d", sum);
 
     return 0;
 }
