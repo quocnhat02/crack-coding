1f19d20d311a9793173427264d02ca7e542487e5
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-06-26 21:37:44 +0700

    sum of digits of n

--- myFirstProg.c
+++ myFirstProg.c
@@ -2,9 +2,19 @@
 
 int main()
 {
+    // 123 / 10 = 12
     int x;
+    int sum = 0;
     scanf("%d", &x);
-    printf("%d\n", x);
+    int temp = x;
+
+    while (temp > 0)
+    {
+        sum = sum + (temp % 10);
+        temp /= 10;
+    }
+
+    printf("%d", sum);
 
     return 0;
 }
