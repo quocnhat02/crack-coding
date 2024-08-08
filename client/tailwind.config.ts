3726cfe9ef699f76b5c166dc09e5ec459b654203
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-08-08 17:40:40 +0700

    navbar

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from 'tailwindcss';
 import { createThemes } from 'tw-colors';
-import colors, { black } from 'tailwindcss/colors';
+import colors from 'tailwindcss/colors';
 
 const baseColors = [
   'gray',
@@ -56,7 +56,7 @@ const themes = {
 const config: Config = {
   darkMode: 'class',
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
